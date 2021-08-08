a9b7921018a4fbbbbbabb3cda2be6368d2a3cf60
ibnsultan <abdulbasitsultan4@gmail.com>
2021-08-08 14:30:58 +0300

    session constants

--- Core/session.php
+++ Core/session.php
@@ -1,13 +1,18 @@
 <?php
+//This page defines the session global constants
+session_start();
 if (!empty($_SESSION)){
-    define('ID',    $_SESSION['id']);
-    define('USER',	$_SESSION['username']);
-    define('NAME',  $_SESSION['fname'].$_SESSION['lname']);
-    define('PHONE', $_SESSION['code'].$_SESSION['phone']);
-	define('ORG',	$_SESSION['organization']);
-	define('BRANCH',$_SESSION['branch']);
-	//define('',		$_SESSION['']);
-	//define('',		$_SESSION['']);
+    define('ID',     $_SESSION['id']);
+    define('USER',	 $_SESSION['username']);
+    define('NAME',   $_SESSION['fname'].$_SESSION['lname']);
+    define('PHONE',  $_SESSION['phone']);
+	define('ORG',	 $_SESSION['organization']);
+	define('BRANCH', $_SESSION['branch']);
+	define('EMAIL',	 $_SESSION['email']);
+	define('PHOTO',	 $_SESSION['photo']);
+	define('NIN',	 $_SESSION['nin']);
+	define('ROLE',	 $_SESSION['role']);
+	define('STATUS', $_SESSION['status']);
 }
 
 ?>

86289e5520cbf4e73d0585fd09e29bdd6ec87747
unknown <abdulbasitsultan4@gmail.com>
2021-08-10 16:16:25 +0300

    modified title header

--- panel/web/login.php
+++ panel/web/login.php
@@ -13,7 +13,7 @@ $config = config();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register :: <?=$config['site']; ?></title>
+    <title>Login :: <?=$config['site']; ?></title>
     <link rel="stylesheet" href="assets/css/style-freedom.css">
 </head>
 
--- panel/web/signup.php
+++ panel/web/signup.php
@@ -10,7 +10,7 @@ $config = config();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login :: <?=$config['site']; ?></title>
+    <title>Register :: <?=$config['site']; ?></title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style-freedom.css">
 </head>

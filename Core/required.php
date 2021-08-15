fb5f760b9f85f3f9bef687fce59d61a7e5a84796
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-15 13:01:50 +0300

    required files designation

---
+++ Core/required.php
@@ -0,0 +1,11 @@
+<?php
+// Global Requirement
+    require_once(CONFIG);
+    require_once(SESSION);
+    require_once(ROUTES);
+
+// control requirements
+    require_once(Ticket.'/ticket.php');
+    require_once(Department.'/department.php');
+
+?>

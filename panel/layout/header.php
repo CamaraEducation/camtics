e3b2ca955acc733f0f53d5567dd769085d88f50b
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-18 14:43:54 +0300

    added a dynamic title

--- panel/layout/header.php
+++ panel/layout/header.php
@@ -13,7 +13,7 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <title><?php echo $config['site']; ?></title>
+        <title><?=$config['site'];?> ::<?php echo str_replace("/"," ", $_SERVER['REQUEST_URI'])?></title>
         <!-- font awesome -->
         <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css">
         <!-- CSS stylesheets -->

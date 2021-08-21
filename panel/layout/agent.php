ca8a63bf53967499ee3dfda8aed01c1bdfd15494
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-21 19:24:07 +0300

    staff navigation menu

---
+++ panel/layout/agent.php
@@ -0,0 +1,18 @@
+<li>
+    <a href="/my/chat">
+        <i class="fas fa-comment-alt"></i>
+        <span>Chat</span> 
+    </a> 
+</li>
+<li>
+    <a href="/my/department">
+        <i class="fas fa-building"></i>
+        <span>Department</span> 
+    </a> 
+</li>
+<li>
+    <a href="/my/branch">
+        <i class="fas fa-code-branch"></i>
+        <span>Branch</span> 
+    </a> 
+</li>

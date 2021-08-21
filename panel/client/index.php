9ceb5765304415e0352b48db7cfd68ef302ea68d
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-21 19:21:49 +0300

    greeting on dashboard insex

--- panel/client/index.php
+++ panel/client/index.php
@@ -13,8 +13,7 @@
 	<!-- content -->
 	<div class="container-fluid content-top-gap">
 		<div class="welcome-msg pt-3 pb-4">
-			<h1>Hi <span class="text-primary">John</span>, Welcome back at <?=$config['site'];?></h1>
-			<p>Very detailed & featured admin.</p>
+			<h1>Hi <span class="text-primary"><?=USER?></span>, Welcome back at <?=$config['site'];?></h1>
 		</div>
 		<!-- statistics data -->
 		<div class="statistics">

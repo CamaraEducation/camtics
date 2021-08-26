19da63fc2207e09a1918c2a3c341c089cf36a811
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-26 15:00:12 +0300

    department menu

---
+++ panel/layout/department.php
@@ -0,0 +1,19 @@
+<li class="menu-list">
+    <a href="#"><i class="fas fa-building"></i>
+        <span>Department</span>
+    </a>
+    <ul class="sub-menu-list">
+        <li>
+            <a href="/department/manage">
+                <i class="fa fa-circle text-primary"></i>
+                Manage 
+            </a> 
+        </li>
+        <li>
+            <a href="/department/users">
+                <i class="fa fa-user text-secondary"></i>
+                Staff 
+            </a>
+        </li>
+    </ul>
+</li>

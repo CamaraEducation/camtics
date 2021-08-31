2039b4885a2a8b8b85df85752b3ebb914367669a
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-08-31 19:55:12 +0300

    bug fixes with sql querries

--- control/ticket/agent.php
+++ control/ticket/agent.php
@@ -4,8 +4,20 @@
  *                                                                      *
  ************************************************************************/
 class StaffTicket extends Ticket{
+    function count_agent_ticket($agent){
+        $count_ticket = "SELECT status as name, COUNT(id) as value FROM ticket WHERE agent='$agent' GROUP BY status";
+        $count_ticket = mysqli_query(conn(), $count_ticket);
+        $rows         = array('open'=>0, 'active'=>0, 'closed'=>0);
+
+        while($row = mysqli_fetch_assoc($count_ticket)){
+            $rows[$row['name']] = $row['value'];
+        }
+
+        return $rows;
+    }
+
     function ass_open_tickets($user){
-        $open_ticket = "SELECT id, subject, DATEDIFF(CURRENT_TIMESTAMP, t.`update`) AS `update` SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='open'";
+        $open_ticket = "SELECT id, subject, DATEDIFF(CURRENT_TIMESTAMP, `update`) AS `update`, SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='open'";
         $open_ticket = mysqli_query(conn(), $open_ticket);
 		$num_tickets = mysqli_num_rows($open_ticket);
 		
@@ -24,7 +36,7 @@ class StaffTicket extends Ticket{
     }
 
     function ass_closed_tickets($user){
-        $open_ticket = "SELECT id, department, agent, subject, SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='closed'";
+        $open_ticket = "SELECT id, subject, DATEDIFF(CURRENT_TIMESTAMP, `update`) AS `update`, SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='closed'";
         $open_ticket = mysqli_query(conn(), $open_ticket);
 		$num_tickets = mysqli_num_rows($open_ticket);
 		
@@ -43,7 +55,7 @@ class StaffTicket extends Ticket{
     }
 
     function ass_active_tickets($user){
-        $open_ticket = "SELECT id, subject, SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='active'";
+        $open_ticket = "SELECT id, subject, DATEDIFF(CURRENT_TIMESTAMP, `update`) AS `update`, SUBSTRING(content, 1, 25) as message, time FROM ticket WHERE agent='$user' AND status='active'";
         $open_ticket = mysqli_query(conn(), $open_ticket);
 		$num_tickets = mysqli_num_rows($open_ticket);
 		

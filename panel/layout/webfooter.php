fa6e9395609421d5b8906d5c9ff28af3e024d310
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-06 11:18:14 +0300

    formatting

--- panel/layout/webfooter.php
+++ panel/layout/webfooter.php
@@ -1,6 +1,5 @@
-<div class="columns copy-right text-right">
-                            <p>© 2019 Placidity. All rights reserved | Design by
-                                <a href="http://w3layouts.com/" target="_blank">W3layouts</a></p>
+                        <div class="columns copy-right text-right">
+                            <p>© <?=date('Y')?>. All rights reserved </p>
                         </div>
                     </div>
                 </div>
@@ -9,14 +8,14 @@
         <!-- //titels-5 -->
         <!-- move top -->
         <button onclick="topFunction()" id="movetop" title="Go to top">
-				<span class="fa fa-long-arrow-up"></span>
-		</button>
+            <span class="fa fa-long-arrow-up"></span>
+        </button>
         <script>
             // When the user scrolls down 20px from the top of the document, show the button
             window.onscroll = function() {
                 scrollFunction()
             };
-
+            
             function scrollFunction() {
                 if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
                     document.getElementById("movetop").style.display = "block";
@@ -24,7 +23,7 @@
                     document.getElementById("movetop").style.display = "none";
                 }
             }
-
+            
             // When the user clicks on the button, scroll to the top of the document
             function topFunction() {
                 document.body.scrollTop = 0;
@@ -32,19 +31,16 @@
             }
         </script>
         <!-- /move top -->
-    </section>
-
-
-    <div id="v-w3layouts"></div>
-    <script>
-        (function(v, d, o, ai) {
-            ai = d.createElement('script');
-            ai.defer = true;
-            ai.async = true;
-            ai.src = v.location.protocol + o;
-            d.head.appendChild(ai);
-        })(window, document, 'http://a.vdo.ai/core/v-w3layouts/vdo.ai.js');
-    </script>
-</body>
-
+        </section>
+        <div id="v-w3layouts"></div>
+        <script>
+            (function(v, d, o, ai) {
+                ai = d.createElement('script');
+                ai.defer = true;
+                ai.async = true;
+                ai.src = v.location.protocol + o;
+                d.head.appendChild(ai);
+            })(window, document, 'http://a.vdo.ai/core/v-w3layouts/vdo.ai.js');
+        </script>
+    </body>
 </html>

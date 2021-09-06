e48edbedb169eef1b52d2fb08343d5eb6349b981
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-06 11:14:45 +0300

    added required classes, mailer and notifier

--- Core/required.php
+++ Core/required.php
@@ -10,5 +10,7 @@
     require_once(Branch.'/branch.php');
     require_once(Chat.'/ticket.php');
     require_once(User.'/staff.php');
+    require_once(Mail.'/smtp.php');
+    require_once(Notifier.'/notification.php');
 
 ?>

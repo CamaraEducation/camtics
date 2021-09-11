c598b7d6ba6e71819a24fb9ac27daf24a6e62d0f
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-11 08:27:35 +0300

    admin specific menu

--- panel/layout/admin.php
+++ panel/layout/admin.php
@@ -41,7 +41,6 @@
         <span>Users</span>
     </a>
     <ul class="sub-menu-list">
-        <li>
             <a href="/list/user">
                 <i class="fa fa-circle text-primary"></i>
                 Manage 

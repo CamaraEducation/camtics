3bd172e6b65a2705a859d4a5fac0c9c68a4d4440
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-13 10:47:42 +0300

    dashboard layout for branch manager

---
+++ panel/branch/index.php
@@ -0,0 +1,68 @@
+<?php 
+	include(_LAYOUT.'/header.php');
+
+	// finding the number of tickets by their specifity
+	$count_ticket   = new Ticket();
+	$num_ticket		= $count_ticket->department_tickets();
+	$total_ticket	= array_sum($num_ticket);
+	$config			= config();
+
+?>
+<!-- main content start -->
+<div class="main-content">
+	<!-- content -->
+	<div class="container-fluid content-top-gap">
+		<div class="welcome-msg pt-3 pb-4">
+			<h1>Hi <span class="text-primary"><?=USER?></span>, Welcome back at <?=$config['site'];?></h1>
+		</div>
+		<!-- statistics data -->
+		<div class="statistics">
+			<div class="row">
+				<div class="col-xl-6 pr-xl-2">
+					<div class="row">
+						<div class="col-sm-6 pr-sm-2 statistics-grid">
+							<div class="card card_border border-primary-top p-4">
+								<i class="lnr lnr-users"> </i>
+								<h3 class="text-primary number"><?=$num_ticket['open'];?></h3>
+								<p class="stat-text">Open Tickets</p>
+							</div>
+						</div>
+						<div class="col-sm-6 pl-sm-2 statistics-grid">
+							<div class="card card_border border-primary-top p-4">
+								<i class="lnr lnr-eye"> </i>
+								<h3 class="text-secondary number"><?=$num_ticket['active'];?></h3>
+								<p class="stat-text">Pending Tickets</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 pl-xl-2">
+					<div class="row">
+						<div class="col-sm-6 pl-sm-2 statistics-grid">
+							<div class="card card_border border-primary-top p-4">
+								<i class="lnr lnr-lock"> </i>
+								<h3 class="text-danger number"><?=$num_ticket['closed'];?></h3>
+								<p class="stat-text">Closed Tickets</p>
+							</div>
+						</div>
+						<div class="col-sm-6 pl-sm-2 statistics-grid">
+							<div class="card card_border border-primary-top p-4">
+								<i class="lnr lnr-envelope"> </i>
+								<h3 class="text-danger number"><?=$total_ticket;?></h3>
+								<p class="stat-text">All Tickets</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!-- modals -->		
+			<?php include (_LAYOUT.'/create-ticket.php'); ?>
+		<!-- //modals -->
+	</div>
+	<!-- //content -->
+</div>
+<!-- main content end-->
+</section>
+<?php include(_LAYOUT.'/footer.php'); ?>
---
+++ panel/branch/ticket-assign.php
@@ -0,0 +1,76 @@
+<!-- Modal -->
+<div class="modal fade" id="assignTicketCenter" tabindex="-1" role="dialog" aria-labelledby="assignTicketCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="assignTicketLongTitle">Assign ticket</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"> 
+        </button>
+      </div>
+      <form id="assignTicket" method="POST">
+        <div class="modal-body">
+            <!-- ticket assigned successfully -->
+				<div class="alert alert-success alert-dismissible" id="assignsuccess" style="display:none;">
+					<a href="#" class="close" data-dismiss="alert" aria-label="close">×</a>
+				</div>
+            <!-- ticket assigned successfully -->
+            <div class="row">
+                <div class="col">
+                    <select class="form-control" id="user" name="user" placeholder="select agent">
+                        <option value="" hidden>Select user</option>
+                        <?php
+                            $department_user = new Staff;
+                            $department_user = $department_user -> department_staff(BRANCH, Department::my_department());
+                            foreach($department_user as $staff){ ?>
+                                <option value='<?=$staff['id']?>'><?=$staff['username']?></option><?php
+                            }
+                        ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" id="butassign" class="btn btn-primary">Save changes</button>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+
+<script>
+	//ajax form submissions
+	    $(document).ready(function () {
+	       $('#butassign').on('click', function () {
+            	$("#butassign").attr("disabled", "disabled");
+	        	var ticket  = "<?=$ticket_id;?>";
+	        	var user    = $('#user').val();
+	           if (message != "") {
+	               $.ajax({
+	                   url: "/assign/ticket",
+                       type: "POST",
+	                    data: {
+	                       ticket: ticket,
+	                       user: user
+	                	},
+	                	cache: false,
+	            	   success: function (dataResult) {
+	        	           var dataResult = JSON.parse(dataResult);
+	                       if (dataResult.statusCode == 200) {
+	                            $("#butassign").removeAttr("disabled");
+                                $("#assignsuccess").show();
+	                            $('#assignsuccess').html('User successfully assigned!');
+	                            setTimeout(function() {
+	                                $( "#assignsuccess" ).hide();
+	                            }, 2000);
+	                        } else if (dataResult.statusCode == 201) {
+	                            alert("Error occured !");
+	                        }
+	                    }
+	                });
+	            } else {
+	                alert('Please fill all the field !');
+	            }
+	        });
+	    });
+</script>

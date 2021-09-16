d545635be976c2f024f3e6ebbf3e3d0b660ed0b5
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-16 13:22:55 +0300

    branch administrator menu

---
+++ panel/layout/branch.php
@@ -0,0 +1,88 @@
+<li class="menu-list">
+    <a href="#"><i class="fas fa-building"></i>
+        <span>Department</span>
+    </a>
+    <ul class="sub-menu-list">
+        <li>
+            <a href="/list/department">
+                <i class="fa fa-circle text-primary"></i>
+                Manage 
+            </a> 
+        </li>
+        <li>
+            <a href="/add/department">
+                <i class="fa fa-plus-circle text-secondary"></i>
+                Add 
+            </a>
+        </li>
+    </ul>
+</li>
+<li class="menu-list">
+    <a href="#"><i class="fas fa-user"></i>
+        <span>Users</span>
+    </a>
+    <ul class="sub-menu-list">
+        <li>
+            <a href="/list/user">
+                <i class="fa fa-circle text-primary"></i>
+                Manage 
+            </a> 
+        </li>
+        <li>
+            <a href="/add/user">
+                <i class="fa fa-plus-circle text-secondary"></i>
+                Add 
+            </a>
+        </li>
+    </ul>
+</li>
+<li class="menu-list">
+    <a href="#"><i class="fas fa-envelope"></i>
+        <span>Mail</span>
+    </a>
+    <ul class="sub-menu-list">
+        <li>
+            <a href="/received/mail">
+                <i class="fa fa-circle text-primary"></i>
+                Incoming
+            </a> 
+        </li>
+        <li>
+            <a href="/send/mail">
+                <i class="fa fa-plus-circle text-secondary"></i>
+                Outgoing 
+            </a>
+        </li>
+    </ul>
+</li>
+<li class="menu-list">
+    <a href="#"><i class="fas fa-comment"></i>
+        <span>SMS</span>
+    </a>
+    <ul class="sub-menu-list">
+        <li>
+            <a href="/received/mail">
+                <i class="fa fa-user text-primary"></i>
+                Individual
+            </a> 
+        </li>
+        <li>
+            <a href="/received/mail">
+                <i class="fa fa-users text-primary"></i>
+                Bulky
+            </a> 
+        </li>
+        <li>
+            <a href="/send/mail">
+                <i class="fa fa-plus-circle text-secondary"></i>
+                History 
+            </a>
+        </li>
+    </ul>
+</li>
+<li>
+    <a href="/setting">
+        <i class="fas fa-cogs"></i>
+        <span>Configurations</span> 
+    </a> 
+</li>

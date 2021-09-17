384007933dfcb16d4e8aabdf6bec9d765adc9cc0
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-17 14:45:35 +0300

    test

--- test.php
+++ test.php
@@ -1,4 +1,25 @@
 <?php
+    /*$server = new Server(
+        $hostname, // required
+        $port,     // defaults to '993'
+        $flags,    // defaults to '/imap/ssl/validate-cert'
+        $parameters
+    );
+
+    $connection = $server->authenticate('', '');
+
+    $message is instance of \Ddeboer\Imap\Message
+		echo $message->getSubject() .'<b>('. $message->getNumber() .')(' .$message->getId(). ')</b><br>';
+		echo $message->getBodyHtml() .'<hr>'; print_r(json_decode(json_encode($message->getHeaders()->get('sender'))))
+		$from = json_decode(json_encode($message->getHeaders()->get('sender')));
+		$from = $from[0];
+		$from = (array) $from;
+		echo '<pre>'; print_r($from['personal']); echo '</pre><hr>';
+		$from = $message->getFrom();
+		echo '<pre>'; print_r($from); echo '</pre>';	    // Message\EmailAddress
+		echo '<pre>'; print_r($message->getTo()); echo '</pre>';      // array of Message\EmailAddress
+		echo '<pre>'; print_r($message->getDate()); echo '</pre>';    // DateTimeImmutable
+
     /*$to = "abdulbasitsultan4@gmail.com";
     $subject = "Testing camtics mail";
          

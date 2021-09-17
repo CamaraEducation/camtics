497f38aa693af5ea33ab0909645989569ffe752e
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-17 19:04:46 +0300

    branch configuration

---
+++ panel/branch/branch-conf.php
@@ -0,0 +1,17 @@
+<?php 
+	include(_LAYOUT.'/header.php')
+?>
+<!-- //header-ends -->
+<!-- main content start -->
+<div class="main-content">
+	<!-- content -->
+	<div class="container-fluid content-top-gap">
+		<section class="bg-light">
+
+		</section>
+	</div>
+	<!-- //content -->
+</div>
+<!-- main content end-->
+</section>
+<?php include(_LAYOUT.'/footer.php') ?>

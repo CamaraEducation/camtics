047bc6c62d8d72bad5e885ab5f21993c5dcddcb1
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-17 14:44:26 +0300

    formatting

--- Core/required.php
+++ Core/required.php
@@ -1,18 +1,18 @@
 <?php
 // Global Requirement
-    require_once(CONFIG);
-    require_once(SESSION);
-    require_once(ROUTES);
+	require_once CONFIG;
+	require_once SESSION;
+	require_once ROUTES;
 
 // control requirements
-    require_once(Ticket.'/ticket.php');
-    require_once(Department.'/department.php');
-    require_once(Branch.'/branch.php');
-    require_once(Chat.'/ticket.php');
-    require_once(User.'/staff.php');
-    require_once(User.'/user.php');
-    require_once(Mail.'/smtp.php');
-    require_once(Notifier.'/notification.php');
-    require_once(Organiztn.'/Organization.php');
+	require_once Ticket     .'/ticket.php';
+	require_once Department .'/department.php';
+	require_once Branch     .'/branch.php';
+	require_once Chat		.'/ticket.php';
+	require_once User		.'/user.php';
+	require_once Mail		.'/smtp.php';
+	require_once Notifier	.'/notification.php';
+	require_once Organiztn	.'/Organization.php';
+	require_once Mail		.'/imap.php';
 
 ?>

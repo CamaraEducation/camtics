9cff3785ff9c01e7a4ac8eea91762786bb049476
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-18 21:17:20 +0300

    function state change, transfer ticket

--- control/ticket/update.php
+++ control/ticket/update.php
@@ -8,7 +8,7 @@ class UpdateTicket extends Ticket{
 
     }
 
-    function reopen($id){
+    public static function activate($id){
         $reopen_ticket = "UPDATE ticket SET status='active' WHERE id='$id'";
         mysqli_query(conn(), $reopen_ticket);
         Ticket::update_ticket($id);
@@ -27,6 +27,16 @@ class UpdateTicket extends Ticket{
     public static function assign($agent, $ticket){
         $assign_ticket = "UPDATE ticket SET agent='$agent' WHERE id='$ticket'";
         if(mysqli_query(conn(), $assign_ticket)){
+			echo json_encode(array("statusCode"=>200));
+		}else{
+			echo json_encode(array("statusCode"=>201));
+		}
+    }
+
+    public static function transfer($department, $ticket){
+        $transfer_ticket = "UPDATE ticket SET department='$department' WHERE id='$ticket'";
+        if(mysqli_query(conn(), $transfer_ticket)){
+            UpdateTicket::activate($ticket);
 			echo json_encode(array("statusCode"=>200));
 		}else{
 			echo json_encode(array("statusCode"=>201));

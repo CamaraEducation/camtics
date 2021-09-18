6b0a3185eedf0e4805c8620aac9380bfd761c874
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-18 21:29:31 +0300

    nothing major

--- control/ticket/update.php
+++ control/ticket/update.php
@@ -4,43 +4,43 @@
  *                                                                      *
  ************************************************************************/
 class UpdateTicket extends Ticket{
-    function escalate(){
+	function escalate(){
 
-    }
+	}
 
-    public static function activate($id){
-        $reopen_ticket = "UPDATE ticket SET status='active' WHERE id='$id'";
-        mysqli_query(conn(), $reopen_ticket);
-        Ticket::update_ticket($id);
-        Notification::ticket_status('ACTIVATED', $id);
-        header('Location: /pending/ticket ');
-    }
-    
-    function close($id){
-        $close_ticket = "UPDATE ticket SET status='closed' WHERE id='$id'"; 
-        mysqli_query(conn(), $close_ticket);
-        Ticket::update_ticket($id);
-        Notification::ticket_status('CLOSED', $id);
-        header('Location: /closed/ticket ');
-    }
+	public static function activate($id){
+		$reopen_ticket = "UPDATE ticket SET status='active' WHERE id='$id'";
+		mysqli_query(conn(), $reopen_ticket);
+		Ticket::update_ticket($id);
+		Notification::ticket_status('ACTIVATED', $id);
+		header('Location: /pending/ticket ');
+	}
+	
+	function close($id){
+		$close_ticket = "UPDATE ticket SET status='closed' WHERE id='$id'"; 
+		mysqli_query(conn(), $close_ticket);
+		Ticket::update_ticket($id);
+		Notification::ticket_status('CLOSED', $id);
+		header('Location: /closed/ticket ');
+	}
 
-    public static function assign($agent, $ticket){
-        $assign_ticket = "UPDATE ticket SET agent='$agent' WHERE id='$ticket'";
-        if(mysqli_query(conn(), $assign_ticket)){
+	public static function assign($agent, $ticket){
+		$assign_ticket = "UPDATE ticket SET agent='$agent' WHERE id='$ticket'";
+		if(mysqli_query(conn(), $assign_ticket)){
 			echo json_encode(array("statusCode"=>200));
 		}else{
 			echo json_encode(array("statusCode"=>201));
 		}
-    }
+	}
 
-    public static function transfer($department, $ticket){
-        $transfer_ticket = "UPDATE ticket SET department='$department' WHERE id='$ticket'";
-        if(mysqli_query(conn(), $transfer_ticket)){
-            UpdateTicket::activate($ticket);
+	public static function transfer($department, $ticket){
+		$transfer_ticket = "UPDATE ticket SET department='$department' WHERE id='$ticket'";
+		if(mysqli_query(conn(), $transfer_ticket)){
+			UpdateTicket::activate($ticket);
 			echo json_encode(array("statusCode"=>200));
 		}else{
 			echo json_encode(array("statusCode"=>201));
 		}
-    }
+	}
 }
 ?>

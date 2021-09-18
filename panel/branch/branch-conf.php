bca82348b561134a998bd4f552cb89b5c6f16bb0
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-18 21:18:25 +0300

    branch configuration

--- panel/branch/branch-conf.php
+++ panel/branch/branch-conf.php
@@ -7,6 +7,33 @@
 	<!-- content -->
 	<div class="container-fluid content-top-gap">
 		<section class="bg-light">
+			<div class="row p-2">
+				<div class="col">
+					<div class="welcome-msg pt-3 pb-4">
+						<h1 class="text-tertiary">Basic Information</h1>
+						<div class="row  p-3">
+							<div class="col form-group">
+								<label for="name">Branch Name</label>
+								<input class='form-control' type="text" name="branch" id="branch" disabled>
+							</div>
+						</div>
+						<div class="row p-3">
+							<div class="col input-group">
+								<span class="input-group-addon">
+									<i class="lnr lnr-phone"></i>
+								</span>
+								<input class="form-control" type="text" name="phone" id="phone">
+							</div>
+							<div class="col"></div>
+						</div>
+					</div>
+				</div>
+				<div class="col">
+				<div class="welcome-msg pt-3 pb-4">
+						<h1 class="text-tertiary">hi</h1>
+					</div>
+				</div>
+			</div>
 
 		</section>
 	</div>

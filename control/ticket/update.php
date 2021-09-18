690587ceb1e073771a181639912dcce394479483
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-18 21:56:59 +0300

    response after ticket update

--- control/ticket/update.php
+++ control/ticket/update.php
@@ -13,7 +13,6 @@ class UpdateTicket extends Ticket{
 		mysqli_query(conn(), $reopen_ticket);
 		Ticket::update_ticket($id);
 		Notification::ticket_status('ACTIVATED', $id);
-		header('Location: /pending/ticket ');
 	}
 	
 	function close($id){
@@ -21,12 +20,12 @@ class UpdateTicket extends Ticket{
 		mysqli_query(conn(), $close_ticket);
 		Ticket::update_ticket($id);
 		Notification::ticket_status('CLOSED', $id);
-		header('Location: /closed/ticket ');
 	}
 
 	public static function assign($agent, $ticket){
 		$assign_ticket = "UPDATE ticket SET agent='$agent' WHERE id='$ticket'";
 		if(mysqli_query(conn(), $assign_ticket)){
+			UpdateTicket::activate($ticket);
 			echo json_encode(array("statusCode"=>200));
 		}else{
 			echo json_encode(array("statusCode"=>201));
@@ -36,7 +35,6 @@ class UpdateTicket extends Ticket{
 	public static function transfer($department, $ticket){
 		$transfer_ticket = "UPDATE ticket SET department='$department' WHERE id='$ticket'";
 		if(mysqli_query(conn(), $transfer_ticket)){
-			UpdateTicket::activate($ticket);
 			echo json_encode(array("statusCode"=>200));
 		}else{
 			echo json_encode(array("statusCode"=>201));

d65dd35233bbe3488b3ef963920636ddcb67e78d
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-19 15:22:49 +0300

    live ajax search for users

--- control/user/user.php
+++ control/user/user.php
@@ -17,6 +17,30 @@ class User{
         }
         return $users;
     }
+
+    public static function search(){
+        if(isset($_REQUEST["term"])){
+            // Prepare a select statement
+            $term = $_REQUEST["term"];
+            $term = mysqli_real_escape_string(conn(), $term);
+            $sql = "SELECT * FROM user WHERE 
+                username LIKE '%$term%' OR
+                fname LIKE '%$term%' OR
+                lname LIKE '%$term%' OR
+                phone LIKE '%$term%' OR
+                email LIKE '%$term%'";
+            $sql = mysqli_query(conn(), $sql);
+            $sql = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
+            if(!empty($sql)){
+                foreach($sql as $user){
+                    echo '<a class="small-hr" href="/view/user/'.$user['username'].'">'.$user['fname'].' '.$user['lname'].'</a>';
+                }
+            }else{
+                echo "nothing found";
+            }
+        }
+    }
 }
 
 include 'staff.php';

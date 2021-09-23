9f538497d7ce57abcb83b5a9b9b3e76ca75657bf
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-23 11:08:05 +0300

    include variables in include file recursively

--- Core/conf.php
+++ Core/conf.php
@@ -29,4 +29,26 @@
         return $rows;
     }
 
+    function include_V($filePath, $variables = array(), $print = true){
+        $output = NULL;
+        if(file_exists($filePath)){
+            // Extract the variables to a local namespace
+            extract($variables);
+
+            // Start output buffering
+            ob_start();
+
+            // Include the template file
+            include $filePath;
+
+            // End buffering and return its contents
+            $output = ob_get_clean();
+        }
+        if ($print) {
+            print $output;
+        }
+        return $output;
+
+    }
+
 ?>

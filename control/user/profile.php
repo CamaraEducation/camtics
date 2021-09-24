93fea494dc00c2eeb6bce795faf8968c77a1ba23
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-09-24 12:05:46 +0300

    profile management class

---
+++ control/user/profile.php
@@ -0,0 +1,34 @@
+<?php
+class UserProfile{
+    public static function edit_basic(){
+        $fname = mysqli_real_escape_string(conn(), $_POST['fname']);
+        $lname = mysqli_real_escape_string(conn(), $_POST['lname']);
+        $phone = mysqli_real_escape_string(conn(), $_POST['phone']);
+        $email = mysqli_real_escape_string(conn(), $_POST['email']);
+        $uname = mysqli_real_escape_string(conn(), $_POST['uname']);
+        $sql = "update user set fname = '$fname', lname = '$lname', phone = '$phone', email = '$email' where username = '$uname'";
+
+        if(mysqli_query(conn(), $sql)){
+			echo json_encode(array('statusCode'=>200));
+        }else{
+			echo json_encode(array('statusCode'=>201));
+        }
+    }
+
+    public static function edit_avatar(){
+        $cpass = md5(mysqli_real_escape_string(conn(), $_POST['cpass']));
+        $npass = md5(mysqli_real_escape_string(conn(), $_POST['npass']));
+        $uname = mysqli_real_escape_string(conn(), $_POST['uname']);
+        $sql = "update user set pass = '$npass' where username = '$uname' and pass='$cpass'";
+        if(mysqli_query(conn(), $sql)){
+			echo json_encode(array('statusCode'=>200));
+        }else{
+			echo json_encode(array('statusCode'=>201));
+        }
+    }
+
+    public static function edit_passcode(){
+
+    }
+}
+?>

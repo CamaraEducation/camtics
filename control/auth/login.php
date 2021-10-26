f16924eab3c71dad0813ce1f783994c5748070a5
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-10-26 12:59:44 +0300

    security issue

--- control/auth/login.php
+++ control/auth/login.php
@@ -1,25 +1,26 @@
 <?php
 class Login{
     function loger($user, $pass){
-		
+		$user	= strtolower(mysqli_real_escape_string(conn(), $user));
+		$pass	= mysqli_real_escape_string(conn(), $pass);
+
         $login  = "SELECT id FROM user WHERE username='$user' AND pass='$pass'";
 		$loger	= mysqli_query(conn(), $login);
 		$loger	= mysqli_fetch_assoc($loger);
 
 		//If user loged create session and redirect to folder.
         if(!empty($loger['id'])){
-			//include(Auth.'/session.php');
 			$loged	= new Session;
-			$loged	-> set_session($user);
-			echo "<script type=\"text/javascript\">
-					alert(\"SUCCESS: Please wait we are loging you in.\");
-					window.location.pathname = \"/\"
-				</script>";
-		}else{
-			echo "<script type=\"text/javascript\">
-					alert(\"Wrong credentials, Please Try Again\");
-					window.location.pathname = \"/login\"
-				</script>";
+			$loged	-> set_session($user); ?>
+			<script type="text/javascript">
+				alert("SUCCESS: Please wait we are loging you in.");
+				window.location.pathname = "/"
+			</script>"; <?php
+		}else{ ?>
+			<script type="text/javascript">
+				alert("Wrong credentials, Please Try Again");
+				window.location.pathname = "/login";
+			</script> <?php
 		}
     }
 

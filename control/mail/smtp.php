a0a3910118ead9a8c90e32cd2fa2866d86ca9657
Abdulbasit Rubeiyya <abdulbasitsultan4@gmail.com>
2021-11-04 08:46:38 +0300

    smtp client added

--- control/mail/smtp.php
+++ control/mail/smtp.php
@@ -1,5 +1,29 @@
 <?php
+use Snipworks\Smtp\Email;
+
 class SendMail{
+    //smtp client
+    public static function init($receiver, $subject, $message){
+        $mail = new Email($_ENV['SMTP_HOST'], $_ENV['SMTP_PORT']);
+        if($_ENV['SMTP_PORT']==587){
+            $mail->setProtocol(Email::TLS);
+        }
+        $mail->setLogin($_ENV['SMTP_USER'], $_ENV['SMTP_PASS']);
+        $mail->addTo($receiver, '');
+        $mail->setFrom($_ENV['SMTP_MAIL'], $_ENV['SMTP_NAME']);
+        $mail->setSubject($subject);
+        $mail->setHtmlMessage($message);
+
+        if($mail->send()){
+            setcookie('notice', 'df_mail_200', time() + (180), "/");
+            echo 'success';
+        } else {
+            setcookie('notice', 'df_mail_201', time() + (180), "/");
+            echo 'fail';
+        }
+    }
+
+    //php sendmail
     public static function send($from, $to, $Bcc, $subject, $message){
         $header  = $from.'\r\n';
         $header .= 'Bcc:'.$Bcc.'\r\n';
